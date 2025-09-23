8204732cfc8afbfffc406de8b8856c569dceb453
Leonardo Estacio <mdnssknght@gmail.com>
2025-09-23 17:50:34 -0500

    Use functions for profit calculator

--- profit-calculator/calculator.go
+++ profit-calculator/calculator.go
@@ -3,24 +3,30 @@ package main
 import "fmt"
 
 func main() {
-	var revenue float64
-	var expenses float64
-	var taxRate float64
+	revenue := promptForFloatingValue("Revenue")
+	expenses := promptForFloatingValue("Expenses")
+	taxRate := promptForFloatingValue("Tax Rate")
 
-	fmt.Print("Revenue: ")
-	fmt.Scan(&revenue)
-
-	fmt.Print("Expenses: ")
-	fmt.Scan(&expenses)
-
-	fmt.Print("Tax Rate: ")
-	fmt.Scan(&taxRate)
-
-	earningsBeforeTax := revenue - expenses
-	earningsAfterTax := earningsBeforeTax * (100 - taxRate) / 100
-	ratio := earningsBeforeTax / earningsAfterTax
+	earningsBeforeTax, earningsAfterTax, ratio := calculateEarningsAndRatio(revenue, expenses, taxRate)
 
 	fmt.Printf("Earnings Before Tax: %.2f\n", earningsBeforeTax)
 	fmt.Printf("Earnings After Tax: %.2f\n", earningsAfterTax)
 	fmt.Printf("Ratio: %.2f\n", ratio)
+}
+
+func promptForFloatingValue(prompt string) float64 {
+	var value float64
+
+	fmt.Printf("%s: ", prompt)
+	fmt.Scan(&value)
+
+	return value
+}
+
+func calculateEarningsAndRatio(revenue, expenses, taxRate float64) (float64, float64, float64) {
+	earningsBeforeTax := revenue - expenses
+	earningsAfterTax := earningsBeforeTax * (100 - taxRate) / 100
+	ratio := earningsBeforeTax / earningsAfterTax
+
+	return earningsBeforeTax, earningsAfterTax, ratio
 }
